4e4d16dcb4092e29476e41bab3b41343ab3c2a0a
Craig Gross <craig@c-gross.net>
2022-12-20 14:48:39 -0500

    Set job_flow to download LODES data

--- src/job_flow.py
+++ src/job_flow.py
@@ -8,8 +8,22 @@ raw_relpath = "../raw_data/"
 processed_path = os.path.join(processed_relpath)
 raw_path = os.path.join(raw_relpath)
 
+# LODES data is too big to store on Github
+lodes_file = raw_path + "md_od_main_JT00_2019.csv"
+if not os.path.isfile(lodes_file):
+    import urllib.request
+    import gzip
+    import shutil
+    print("Downloading LODES data into " + lodes_file)
+    url = "https://lehd.ces.census.gov/data/lodes/LODES7/md/od/md_od_main_JT00_2019.csv.gz"
+    urllib.request.urlretrieve(url, raw_path + "tmp.gz")
+    with gzip.open(raw_path + "tmp.gz", "rb") as f_in:
+        with open(lodes_file, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove(raw_path + "tmp.gz")
+    
 
-job_data = pd.read_csv(raw_path + "md_od_main_JT00_2019.csv")
+job_data = pd.read_csv(lodes_file)
 
 # extract census tract
 job_data["h_geocode"] = job_data["h_geocode"].apply(lambda x: str(x)[:11])
